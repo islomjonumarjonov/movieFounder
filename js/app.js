4ff563ede09b253da3c34ebb248784f82dada243
islomumarjonov <islomjon.umarjonov2@gmail.com>
2023-08-03 01:26:12 +0500

    tiny changes

--- js/app.js
+++ js/app.js
@@ -5,24 +5,21 @@ const modal = document.querySelector(".modal");
 const header = document.querySelector(".header__container");
 const main = document.querySelector(".main__container");
 
+console;
+
 const KEY = "263d22d8";
 const ul = document.querySelector("ul");
 
-title.value.toLowerCase();
+title.value.toLowerCase().replaceAll(" ", "+");
 type.value.toLowerCase();
 // btn
 const searchBtn = document.querySelector(".search-btn");
 const randomBtn = document.querySelector(".random-btn");
 const searchAgainBtn = document.querySelector(".search-icon");
-// ul.innerHTML = `
-//   <button id="readmore-btn"></button>
-// `;
 
 randomBtn.addEventListener("click", () => {
   const randomTitle = Math.floor(Math.random() * random.length) - 1;
-  console.log(randomTitle);
   let newTitle = random[randomTitle].toLowerCase().replaceAll(" ", "+");
-  console.log(newTitle);
   finder(newTitle).then((data) =>
     data.Search.forEach((item) => {
       updateUI(item);
@@ -43,7 +40,6 @@ const finder = async (data, year = "", type = "") => {
 const reader = async (data) => {
   header.classList.add("hidden");
   main.classList.add("hidden");
-  console.log("1");
   const query = "https://www.omdbapi.com/?apikey=";
   const ggle = `${KEY}&i=${data.imdbID}`;
   const req = await fetch(query + ggle);
@@ -51,8 +47,6 @@ const reader = async (data) => {
   return result;
 };
 const updateUI = (smth) => {
-  console.log(smth);
-  console.log(smth.imdbID);
   ul.innerHTML += `
         <li class="updated">
         <img src="${smth.Poster}" alt="">
@@ -109,70 +103,3 @@ searchAgainBtn.addEventListener("click", (item) => {
   modal.classList.remove("hidden");
   searchAgainBtn.classList.add("hidden");
 });
-
-//!
-// changeLocation.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const cityName = changeLocation.city.value.trim();
-//   changeLocation.reset();
-//   getWeather(cityName).then((data) => updateUI(data));
-// });
-
-// const KEY = "082855b6ab9c5af85d7eb9fc8d611808";
-
-// const getData = async (city) => {
-//   const base = "https://api.openweathermap.org/data/2.5/weather";
-//   const query = `?q=${city}&appid=${KEY}`;
-//   loader(true);
-//   const req = await fetch(base + query);
-//   const data = await req.json();
-//   loader(false);
-//   return data;
-// };
-
-///
-
-// const changeLocation = document.getElementById("change-location");
-// const card = document.getElementById("card");
-// const details = document.getElementById("details");
-// const weatherIcon = document.getElementById("weather-icon");
-// const overlay = document.getElementById("overlay");
-
-// changeLocation.city.focus();
-
-// const loader = (state) => {
-//   if (state) {
-//     overlay.classList.remove("d-none");
-//   } else {
-//     overlay.classList.add("d-none");
-//   }
-// };
-
-// const updateUI = (weather) => {
-//   let temp = Math.floor(weather.main.temp);
-//   temp -= Math.floor(weather.main.temp) % 10;
-//   let tem = "";
-//   tem += temp.toString().charAt(0);
-//   tem += temp.toString().charAt(1);
-//   details.innerHTML = `
-//     <h5 class="mb-3">${weather.name}, ${weather.sys.country}</h5>
-//     <p class="mb-3">${weather.weather[0].main}</p>
-//     <div class="display-4 mb-3">
-//       <span>${tem}</span>
-//       <span>&deg;C</span>
-//     </div>
-//     </div>
-//     `;
-
-//   if (card.classList.contains("d-none")) {
-//     card.classList.remove("d-none");
-//   }
-
-//   weatherIcon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
-// };
-
-// const getWeather = async (city) => {
-//   const data = getData(city);
-
-//   return data;
-// };
